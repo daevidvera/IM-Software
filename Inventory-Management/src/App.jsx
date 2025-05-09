41b1d575fedb905f4d9fdaf8cbaa69de9d553217
Carlos David Rivera <carlosrescoto04@gmail.com>
2025-05-08 18:05:24 -0600

    finish signup/login/verification backend

--- Inventory-Management/src/App.jsx
+++ Inventory-Management/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePageComponent/WelcomePage';
 import Login from './components/LoginComponent/Login';
 import SignIn from './components/SignInComponent/SignIn';
+import Verification from './components/VerificationComponent/Verification';
+
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
         <Route path ='/' element={<WelcomePage/>}/>
         <Route path ='/login' element={<Login/>}/>
         <Route path ='/signin' element={<SignIn/>}/>
+        <Route path ='/verify' element={<Verification/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- Inventory-Management/src/components/SignInComponent/SignIn.jsx
+++ Inventory-Management/src/components/SignInComponent/SignIn.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './SignIn.css'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+  const navigate = useNavigate()
+  const handleBack = () => {
+    navigate("/")
+  }
+
+  const handleSignIn = () => {
+    navigate("/verify")
+  }
+
   return (
-    <div>SignIn</div>
+      <>
+        <div className="position-absolute top-5 start-7 m-3">
+          <i className="bi bi-arrow-left icon-black" onClick={handleBack} ></i>
+        </div>
+  
+  
+      <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+       <div className="container text-center">
+          <div className="row gap-3 justify-content-center">
+            <h2 className="title col-12">Inventory.</h2>
+            <div className="col-12 col-md-6">
+              <form>
+              <div className="mb-3">
+                  <input type="username" className="form-control border-color black" id="exampleInputUsername" placeholder='Username' />
+                </div>
+                <div className="mb-3">
+                  <input type="email" className="form-control border-color black" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email' />
+                </div>
+                <div className="mb-3">
+                  <input type="password" className="form-control border-color" id="exampleInputPassword1" placeholder='Password' />
+                </div>
+                <div className="mb-3">
+                  <input type="password" className="form-control border-color" id="exampleInputPassword2" placeholder=' Re-Type Password' />
+                </div>
+                <button type="submit" className="btn-color dark w-100 mb-3" onClick={handleSignIn}>Sign In</button>
+              </form>      
+            </div>
+          </div>
+        </div>
+     </div>
+     </>
   )
 }
 
