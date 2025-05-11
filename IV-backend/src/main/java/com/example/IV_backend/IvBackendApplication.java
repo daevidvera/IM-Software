aabd29a0ca6e3571d9ff7a77890ae9687055c16b
David Rivera <crivera62@gatech.edu>
2025-05-10 21:51:23 -0400

    env variables

--- IV-backend/src/main/java/com/example/IV_backend/IvBackendApplication.java
+++ IV-backend/src/main/java/com/example/IV_backend/IvBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.IV_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IvBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
+		System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(IvBackendApplication.class, args);
 	}
 
